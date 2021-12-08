e088fdbac705e0e4b2f1a0a1de491e90f8c4c0d9
Chris Thielen <cthielen@netflix.com>
2021-12-07 21:46:09 -1000

    day 8 part 1

--- day8/part1.ts
+++ day8/part1.ts
@@ -5,50 +5,9 @@ const inputPath = path.resolve(__dirname, "input.txt");
 const input = fs.readFileSync(inputPath, "utf-8");
 
 const lines = input.split(/[\r\n]/).filter((line) => !!line);
+const outputStrs = lines
+  .map((line) => line.split(" | ")[1].split(/ +/))
+  .flat()
+  .filter((signal) => [2, 3, 4, 7].includes(signal.length));
 
-class LedDisplay {
-  constructor(line: string) {
-    const [signalStr, digitStr] = line.split(" | ");
-    // this.signals =
-  }
-}
-
-type X = 0 | 1 | 2 | 3 | 4 | 5;
-type Y = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
-type Coord = [X, Y];
-
-const knownDigits = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"];
-
-class Digit {
-  private segments: string[];
-
-  constructor(segments: string) {
-    this.segments = segments.split("").sort();
-  }
-
-  toString() {
-    // prettier-ignore
-    const segmentCoords: { [key: string]: Coord[] } = {
-      a: [[1, 0], [2, 0], [3, 0], [4, 0]],
-      b: [[0, 1], [0, 2]],
-      c: [[5, 1], [5, 2]],
-      d: [[1, 3], [2, 3], [3, 3], [4, 3]],
-      e: [[0, 4], [0, 5]],
-      f: [[5, 4], [5, 5]],
-      g: [[1, 6], [2, 6], [3, 6], [4, 6]]
-    };
-
-    const output = new Array(7).fill("").map(() => "      ".split(""));
-    this.segments.forEach((s) => {
-      segmentCoords[s].forEach(([x, y]) => {
-        output[y][x] = s;
-      });
-    });
-
-    return output.map((line) => line.join("")).join("\n");
-  }
-}
-
-for (let i = 0; i < 10; i++) {
-  console.log(new Digit(knownDigits[i]).toString());
-}
+console.log(outputStrs.length);
