5ccaf6e7ff91598e8db042f99525deee59013f80
Chris Thielen <cthielen@netflix.com>
2021-12-08 20:05:30 -1000

    simplify: day 9 part 1

--- day9/part1.ts
+++ day9/part1.ts
@@ -9,33 +9,17 @@ const lineLen = lines[0].length;
 
 const lineAsNumberArray = (line: string) => line.split("").map((x) => parseInt(x, 10));
 const grid = lines.map((line) => lineAsNumberArray(line));
-const neighborsx = (y: number, x: number) => {
-  if (x === 0) {
-    return [grid[y][x + 1]];
-  } else if (x === lineLen - 1) {
-    return [grid[y][x - 1]];
-  } else {
-    return [grid[y][x - 1], grid[y][x + 1]];
-  }
-};
 
-const neighborsy = (y: number, x: number) => {
-  if (y === 0) {
-    return [grid[y + 1][x]];
-  } else if (y === lines.length - 1) {
-    return [grid[y - 1][x]];
-  } else {
-    return [grid[y - 1][x], grid[y + 1][x]];
-  }
+const neighbors = (y: number, x: number) => {
+  const values = [grid[y][x - 1], grid[y][x + 1], grid[y - 1]?.[x], grid[y + 1]?.[x]];
+  return values.filter((x) => x !== undefined);
 };
 
 let totalRisk = 0;
 for (let x = 0; x < lineLen; x++) {
   for (let y = 0; y < lines.length; y++) {
     const val = grid[y][x];
-    const neighbors = [...neighborsx(y, x), ...neighborsy(y, x)];
-    if (neighbors.every((n) => val < n)) {
-      // console.log({ x, y, val, neighbors });
+    if (neighbors(y, x).every((n) => val < n)) {
       totalRisk += 1 + val;
     }
   }
