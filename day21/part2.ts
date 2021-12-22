697c4def5adf33547047db28893586260e2905e7
Chris Thielen <cthielen@netflix.com>
2021-12-22 13:50:23 -1000

    day 21 part 2 - use memo

--- day21/part2.ts
+++ day21/part2.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import { range } from "lodash";
+import { memoize, range } from "lodash";
 import * as path from "path";
 
 const inputPath = path.resolve(__dirname, "input.txt");
@@ -25,35 +25,24 @@ const outcomes = range(0, 10).map(roll3);
 type P1Wins = number;
 type P2Wins = number;
 type Result = [P1Wins, P2Wins];
-const resultCache: { [key: string]: Result } = {};
-
 function playTurn(player: 1 | 0, p1pos: number, p2pos: number, p1score: number, p2score: number): Result {
-  const key = [player, p1pos, p2pos, p1score, p2score].join();
-  const cachedResult = resultCache[key];
-  if (cachedResult) {
-    return cachedResult;
-  }
-
   if (p1score >= WINNING_SCORE) {
     return [1, 0];
   } else if (p2score >= WINNING_SCORE) {
     return [0, 1];
   }
 
-  const cumulativeResult: Result = outcomes[player === 0 ? p1pos : p2pos].reduce(
-    (acc, outcome) => {
-      // prettier-ignore
-      const result = player === 0 ?
-        playTurn(1, outcome, p2pos, p1score + (outcome + 1), p2score) :
-        playTurn(0, p1pos, outcome, p1score, p2score + (outcome + 1));
-
-      return [acc[0] + result[0], acc[1] + result[1]] as Result;
+  return outcomes[player === 0 ? p1pos : p2pos].reduce(
+    ([p1wins, p2wins], outcome) => {
+      const [morep1wins, morep2wins] =
+        player === 0
+          ? play(1, outcome, p2pos, p1score + (outcome + 1), p2score)
+          : play(0, p1pos, outcome, p1score, p2score + (outcome + 1));
+      return [p1wins + morep1wins, p2wins + morep2wins] as Result;
     },
     [0, 0]
   );
-
-  resultCache[key] = cumulativeResult;
-  return cumulativeResult;
 }
 
-console.log(playTurn(0, positions[0], positions[1], 0, 0));
+const play = memoize(playTurn, (...args) => args.join());
+console.log(play(0, positions[0], positions[1], 0, 0));
