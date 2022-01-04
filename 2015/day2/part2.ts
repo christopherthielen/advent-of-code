ff66ff9283db0fc240410c6a1a455dcecfad6e72
Chris Thielen <cthielen@netflix.com>
2022-01-03 22:32:28 -1000

    2015.2.2

--- 2015/day2/part2.ts
+++ 2015/day2/part2.ts
@@ -1,17 +1,10 @@
-import { readLines } from "../util";
+import { readLines, toInt, combine } from "../util";
 
-const input: any[] = readLines("input.txt")[0]
-  .split("")
-  .map((c) => (c === "(" ? 1 : c === ")" ? -1 : c));
+const input = readLines("input.txt").map((line) => line.split("x").map(toInt)) as Array<[number, number, number]>;
 
-function basement() {
-  let acc = 0;
-  for (let i = 0; i < input.length; i++) {
-    acc += input[i];
-    if (acc < 0) {
-      return i + 1;
-    }
-  }
-}
+const total = input.reduce((acc, [l, h, w]) => {
+  const perimiters = combine([l, h, w], 2).map(([x, y]) => x + x + y + y);
+  return acc + l * h * w + Math.min(...perimiters);
+}, 0);
 
-console.log(basement());
+console.log({ total });
