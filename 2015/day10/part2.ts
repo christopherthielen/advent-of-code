8c953d9e72bed9db6faf49627f0e6286c6dbbcab
Chris Thielen <cthielen@netflix.com>
2022-01-04 11:43:26 -1000

    2015.10.2

---
+++ 2015/day10/part2.ts
@@ -0,0 +1,24 @@
+import { permutations, readLines, toInt, uniqR } from "../util";
+
+let input = "1113222113";
+
+for (let i = 0; i < 50; i++) {
+  // console.log(input);
+  input = process(input).join("");
+}
+console.log(input.length);
+
+function process(str: string) {
+  const stack = [];
+  str
+    .split("")
+    .map(toInt)
+    .forEach((char, idx) => {
+      if (char === stack[stack.length - 1]) {
+        stack[stack.length - 2] = stack[stack.length - 2] + 1;
+      } else {
+        stack.push(1, char);
+      }
+    });
+  return stack;
+}
