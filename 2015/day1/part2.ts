fc2dbb74814db66c5686108b0e6c800866d56e43
Chris Thielen <cthielen@netflix.com>
2022-01-03 16:06:12 -1000

    2015.1.2

---
+++ 2015/day1/part2.ts
@@ -0,0 +1,15 @@
+import { readLines } from "../util";
+
+const input: any[] = readLines("input.txt")[0].split("").map(c => c === "(" ? 1 : c === ")" ? -1 : c);
+
+function basement() {
+  let acc = 0;
+  for (let i = 0; i < input.length; i++) {
+    acc += input[i];
+    if (acc < 0) {
+      return i + 1;
+    }
+  }
+}
+
+console.log(basement());
