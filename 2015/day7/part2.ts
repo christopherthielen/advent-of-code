95508bb903fae901dc5a8b31fdb2c2b957c2cd82
Chris Thielen <cthielen@netflix.com>
2022-01-04 00:38:56 -1000

    2015.7.2

--- 2015/day7/part2.ts
+++ 2015/day7/part2.ts
@@ -23,42 +23,32 @@ const RShift = (a: string, shift: number, wire: string): Gate => ({
 const Not = (a: string, wire: string): Gate => ({ wire, get: memoize(() => mask(~w(a).get())) });
 const Wire = (a: string, wire: string): Gate => ({ wire, get: memoize(() => mask(w(a).get())) });
 
-const Debug = (gate: Gate, line: string, idx: number): Gate => {
-  return {
-    wire: gate.wire,
-    get: () => {
-      const result = gate.get();
-      // console.log(idx, line, result);
-      return result;
-    },
-  };
-};
-
 const gates: Gate[] = readLines("input.txt").map((line, idx) => {
   const [instruction, wire] = line.split(" -> ");
   if (instruction.includes(" AND ")) {
     const [a, b] = instruction.split(" AND ");
-    return Debug(And(a, b, wire), line, idx);
+    return And(a, b, wire);
   } else if (instruction.includes(" OR ")) {
     const [a, b] = instruction.split(" OR ");
-    return Debug(Or(a, b, wire), line, idx);
+    return Or(a, b, wire);
   } else if (instruction.includes(" LSHIFT ")) {
     const [a, b] = instruction.split(" LSHIFT ");
-    return Debug(LShift(a, toInt(b), wire), line, idx);
+    return LShift(a, toInt(b), wire);
   } else if (instruction.includes(" RSHIFT ")) {
     const [a, b] = instruction.split(" RSHIFT ");
-    return Debug(RShift(a, toInt(b), wire), line, idx);
+    return RShift(a, toInt(b), wire);
   } else if (instruction.includes("NOT ")) {
     const [_, a] = instruction.split("NOT ");
-    return Debug(Not(a, wire), line, idx);
+    return Not(a, wire);
   } else if (instruction.match(/^\d+$/)) {
-    return Debug(Constant(toInt(instruction), wire), line, idx);
+    return Constant(toInt(instruction), wire);
   } else if (instruction.match(/^[a-z]+$/)) {
-    return Debug(Wire(instruction, wire), line, idx);
+    return Wire(instruction, wire);
   } else {
     throw new Error(line);
   }
 });
 
+w("b").get = () => 46065;
 console.log("---------------------------");
 console.log(w("a").get());
