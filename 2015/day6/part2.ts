db389d742a6d0c27ab528c91300ab0ae416faaef
Chris Thielen <cthielen@netflix.com>
2022-01-03 23:48:56 -1000

    2015.6.2

--- 2015/day6/part2.ts
+++ 2015/day6/part2.ts
@@ -4,15 +4,16 @@ const lines = readLines("input.txt")
   .map((line) => /(toggle|turn on|turn off) (\d+),(\d+) through (\d+),(\d+)/.exec(line))
   .map((match) => {
     const [x1, y1, x2, y2] = match.slice(2).map(toInt);
-    return { val: match[1], x1, y1, x2, y2 };
+    const val = match[1] === "toggle" ? 2 : match[1] === "turn on" ? 1 : -1;
+    return { val, x1, y1, x2, y2 };
   });
 
-const grid = new Array(1000).fill(0).map(() => new Array(1000).fill(false));
+const grid = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
 lines.forEach((line) =>
   range(line.x1, line.x2).forEach((x) =>
     range(line.y1, line.y2).forEach((y) => {
-      grid[y][x] = line.val === "toggle" ? !grid[y][x] : line.val === "turn on";
+      grid[y][x] = Math.max(0, grid[y][x] + line.val);
     })
   )
 );
-console.log(grid.flat().filter((x) => x).length);
+console.log(grid.flat().reduce((acc, x) => acc + x, 0));
