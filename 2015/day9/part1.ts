9860162df45a6bfd26112448c39c61e23724d4d8
Chris Thielen <cthielen@netflix.com>
2022-01-04 10:58:20 -1000

    2015.9.1

---
+++ 2015/day9/part1.ts
@@ -0,0 +1,34 @@
+import { permutations, readLines, uniqR } from "../util";
+
+const lines = readLines("input.txt");
+const segments = lines.map((raw) => {
+  const [segment, cost] = raw.split(" = ");
+  const [from, to] = segment.split(" to ");
+  return { from, to, cost: parseInt(cost, 10) };
+});
+
+const costs: { [node: string]: { [node: string]: number } } = {};
+segments.forEach((s) => {
+  const from = (costs[s.from] = costs[s.from] ?? {});
+  const to = (costs[s.to] = costs[s.to] ?? {});
+  from[s.to] = s.cost;
+  to[s.from] = s.cost;
+});
+
+const nodes = segments
+  .map((s) => [s.to, s.from])
+  .flat()
+  .reduce(uniqR, []);
+
+let best = [[], Number.MAX_SAFE_INTEGER];
+permutations(nodes).forEach((nodes) => {
+  const cost = nodes.reduce((acc, node, idx) => {
+    const hopCost = costs[node][nodes[idx + 1]];
+    return hopCost ? acc + hopCost : acc;
+  }, 0);
+  if (cost < best[1]) {
+    best = [nodes, cost];
+  }
+});
+
+console.log(best);
--- 2015/util.ts
+++ 2015/util.ts
@@ -1,14 +1,7 @@
 import * as fs from "fs";
 
-export const range = (start: number, end: number) => {
-  const low = Math.min(start, end);
-  const high = Math.max(start, end);
-  const result = [];
-  for (let i = low; i <= high; i++) {
-    result.push(i);
-  }
-  return start < end ? result : result.reverse();
-};
+export const toInt = (str: string) => parseInt(str, 10);
+export const without = <T>(items: T[], item: T) => items.filter((x) => x !== item);
 export const uniqR = <T>(acc: T[], item: T): T[] => (acc.includes(item) ? acc : acc.concat(item));
 export const combine2 = <T>(items: T[]) => combine(items, 2) as Array<[T, T]>;
 export const combine3 = <T>(items: T[]) => combine(items, 3) as Array<[T, T, T]>;
@@ -21,12 +14,26 @@ export const combine = <T>(items: T[], count: number): Array<T[]> => {
   }, []);
 };
 
-export const toInt = (str: string) => parseInt(str, 10);
+export const permutations = <T>(items: T[]): Array<T[]> => {
+  if (items.length === 2) return [items.slice(), items.reverse()];
+  return items.map((item, idx) => permutations(without(items, item)).map((sublist) => [item, ...sublist])).flat();
+};
+
+export const range = (start: number, end: number) => {
+  const low = Math.min(start, end);
+  const high = Math.max(start, end);
+  const result = [];
+  for (let i = low; i <= high; i++) {
+    result.push(i);
+  }
+  return start < end ? result : result.reverse();
+};
 
 export class Counter extends Map {
   constructor() {
     super();
   }
+
   count(key: string) {
     if (!this.has(key)) {
       this.set(key, 1);
