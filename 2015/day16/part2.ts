8108e6926b3ce9f973cd4fede87ad0734f9f5ef7
Chris Thielen <cthielen@netflix.com>
2022-01-05 00:22:50 -1000

    2015.16.2

---
+++ 2015/day16/part2.ts
@@ -0,0 +1,43 @@
+import { permutations, readLines, uniqR } from "../util";
+
+const lines = readLines("input.txt");
+const sues = lines.map((line) => {
+  // Sue 4: perfumes: 2, vizslas: 0, cars: 6
+  const [_, sue, rest] = /Sue (\d+): (.*)/.exec(line);
+  const clues = Object.fromEntries(
+    rest.split(", ").map((clue) => {
+      const [key, value] = clue.split(": ");
+      return [key, parseInt(value, 10)];
+    })
+  );
+  return { sue: parseInt(sue, 10), clues };
+});
+
+const facts = {
+  children: 3,
+  cats: 7,
+  samoyeds: 2,
+  pomeranians: 3,
+  akitas: 0,
+  vizslas: 0,
+  goldfish: 5,
+  trees: 3,
+  cars: 2,
+  perfumes: 1,
+};
+
+const matches = sues.filter((sue) => {
+  return Object.entries(facts).every(([key, value]) => {
+    switch (key) {
+      case "cats":
+      case "trees":
+        return !sue.clues.hasOwnProperty(key) || sue.clues[key] > value;
+      case "pomeranians":
+      case "goldfish":
+        return !sue.clues.hasOwnProperty(key) || sue.clues[key] < value;
+    }
+    return !sue.clues.hasOwnProperty(key) || sue.clues[key] === value;
+  });
+});
+
+console.log(matches);
