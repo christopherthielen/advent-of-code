6cfbd7fd1641ccea523bc0c451ed8e53d7a50f5b
Chris Thielen <cthielen@netflix.com>
2022-01-05 17:20:10 -1000

    2015.20.2

---
+++ 2015/day20/part2.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+
+function presentsForHouse(house: number) {
+  const factors = [];
+  let max = house;
+  for (let i = 1; i <= max; i++) {
+    if (house % i === 0) {
+      factors.push(i, house / i);
+      max = house / i;
+    }
+  }
+  return (
+    factors
+      .filter((factor) => {
+        return factor * 50 >= house;
+      })
+      .reduce((acc, x) => acc + x) * 11
+  );
+}
+
+const MIN_PRESENTS = 34000000;
+let ticks = Date.now();
+let house = 1;
+let max = 0;
+while (true) {
+  let number = presentsForHouse(house++);
+  max = Math.max(number, max);
+  if (number >= MIN_PRESENTS) {
+    break;
+  }
+  if (Date.now() - ticks > 1000) {
+    console.log(`[max: ${max}] House ${house++} got ${number} presents.`);
+    ticks = Date.now();
+  }
+}
+
+console.log(`House ${--house} got ${presentsForHouse(house)} presents.`);
