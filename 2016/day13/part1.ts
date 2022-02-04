3002c63cf9c4121fcdae97f81a197164bac671c2
Chris Thielen <cthielen@netflix.com>
2022-02-03 19:51:49 -1000

    2016.12.2

--- 2016/day13/part1.ts
+++ 2016/day13/part1.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert";
 import { array2d, Grid, Item } from "../grid";
 
 declare module "../grid" {
@@ -12,7 +13,7 @@ const dest = { x: 31, y: 39 };
 // const favnum = 10;
 // const dest = { x: 7, y: 4 };
 
-const grid = new Grid(array2d(50, 50, "."));
+const grid = new Grid(array2d(52, 52, "."));
 grid.items.flat().forEach((item) => {
   const { x, y } = item;
   const val = x * x + 3 * x + 2 * x * y + y + y * y + favnum;
@@ -23,12 +24,11 @@ grid.items.flat().forEach((item) => {
 });
 const neighbors = ({ n, e, w, s }: Item<string>) => [n, e, w, s].filter((x) => x);
 
-console.log(grid.toString());
-
 const start = grid.items[1][1];
 start.cost = 0;
 const queue = [start];
 
+// part 1
 while (queue.length) {
   const item = queue.shift();
   if (item.x === dest.x && item.y === dest.y) {
@@ -44,3 +44,16 @@ while (queue.length) {
       queue.push(n);
     });
 }
+
+// part 2
+while (queue.length) {
+  const item = queue.shift();
+  neighbors(item)
+    .filter((n) => n.val !== "#")
+    .filter((n) => n.cost === undefined || n.cost > item.cost + 1)
+    .forEach((n) => {
+      n.cost = item.cost + 1;
+      queue.push(n);
+    });
+}
+console.log(grid.items.flat().filter((item) => item.cost !== undefined && item.cost <= 50).length);
