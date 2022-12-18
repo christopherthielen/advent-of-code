d3f7e644a9ee57c5f6e45f8a91e8fe3e6744b8ea
Chris Thielen <github@sandgnat.com>
2022-12-17 14:17:20 -1000

    feat(perf): Added a function perf reporter: count/elapsed/average/persec

---
+++ 2022/perf.ts
@@ -0,0 +1,38 @@
+import { Counter, lpad } from "./util";
+
+export function perf(fn: Function, interval = 1000) {
+  const micros = () => {
+    const hrTime = process.hrtime();
+    return hrTime[0] * 1000000 + hrTime[1] / 1000;
+  };
+  const counter = new Counter();
+  const name = fn.name;
+  const COUNT = name + ".count";
+  const ELAPSED = name + ".elapsed";
+
+  let lastReport = Date.now();
+
+  function report() {
+    if (Date.now() - lastReport > interval) {
+      const count = counter.get(COUNT);
+      const elapsed = counter.get(ELAPSED);
+
+      const c = lpad(20, "" + count);
+      const e = lpad(20, "" + elapsed.toFixed(3));
+      const avg = lpad(20, "" + (elapsed / count).toFixed(3));
+      const persec = lpad(20, "" + (1000000 / (elapsed / count)).toFixed(3));
+      console.log(`${lpad(12, name)} count: ${c} elapsed: ${e} µs avg: ${avg} µs ${persec}/sec`);
+      lastReport = Date.now();
+    }
+  }
+
+  return function () {
+    const tick = micros();
+    const result = fn.apply(this, arguments);
+    const delta = micros() - tick;
+    counter.count(name + ".count");
+    counter.count(name + ".elapsed", delta);
+    report();
+    return result;
+  };
+}
