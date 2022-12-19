2a682383b5f9d0651b52e01413d74c76cd95f4b0
Chris Thielen <github@sandgnat.com>
2022-12-19 08:50:19 -1000

    feat(perf): allow passing fnname in

--- 2022/perf.ts
+++ 2022/perf.ts
@@ -1,14 +1,13 @@
 import { Counter, lpad } from "./util";
 
-export function perf<ARG extends Function>(fn: ARG, interval = 1000): ARG {
+export function perf<ARG extends Function>(fn: ARG, interval = 1000, fname = fn.name): ARG {
   const micros = () => {
     const hrTime = process.hrtime();
     return hrTime[0] * 1000000 + hrTime[1] / 1000;
   };
   const counter = new Counter();
-  const name = fn.name;
-  const COUNT = name + ".count";
-  const ELAPSED = name + ".elapsed";
+  const COUNT = fname + ".count";
+  const ELAPSED = fname + ".elapsed";
 
   let lastReport = Date.now();
 
@@ -21,7 +20,7 @@ export function perf<ARG extends Function>(fn: ARG, interval = 1000): ARG {
       const e = lpad(20, "" + elapsed.toFixed(3));
       const avg = lpad(20, "" + (elapsed / count).toFixed(3));
       const persec = lpad(20, "" + (1000000 / (elapsed / count)).toFixed(3));
-      console.log(`${lpad(12, name)} count: ${c} elapsed: ${e} µs avg: ${avg} µs ${persec}/sec`);
+      console.log(`${lpad(12, fname)} count: ${c} elapsed: ${e} µs avg: ${avg} µs ${persec}/sec`);
       lastReport = Date.now();
     }
   }
@@ -30,8 +29,8 @@ export function perf<ARG extends Function>(fn: ARG, interval = 1000): ARG {
     const tick = micros();
     const result = fn.apply(this, arguments);
     const delta = micros() - tick;
-    counter.count(name + ".count");
-    counter.count(name + ".elapsed", delta);
+    counter.count(fname + ".count");
+    counter.count(fname + ".elapsed", delta);
     report();
     return result;
   } as unknown as ARG;
