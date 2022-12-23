70f85f915a1e91aa4eb4b9b13e48de0b0a7438b3
Chris Thielen <github@sandgnat.com>
2022-12-23 09:12:26 -1000

    2022.16.2 (working on a new approach)

--- 2022/day16/part2.ts
+++ 2022/day16/part2.ts
@@ -1,7 +1,7 @@
-import { PERF, perf, showPerf } from "../perf";
-import { PQ } from "../priorityQueue";
-import { range, readLines, toInt, without } from "../util";
+import { isEqual, uniqBy } from "lodash";
 import * as yargs from "yargs";
+import { PERF, perf } from "../perf";
+import { range, readLines, toInt, without } from "../util";
 
 yargs.option("perf", { boolean: true }).option("example", { boolean: true });
 
@@ -9,15 +9,19 @@ PERF.enabled = !!yargs.argv["perf"];
 const TICKS = 26;
 const INPUT = yargs.argv["example"] ? "example.txt" : "input.txt";
 
-type ValveInfo = {
+type Valve = {
   name: string;
   flowPerMin: number;
-  neighbors: ValveInfo[];
-  distance: { [key: string]: number };
-  tmax: { [dest: string]: number[] };
+  neighbors: Valve[];
 };
 
-const parseLine = (line: string): ValveInfo & { rawNeighbors: string[] } => {
+type Distances = {
+  [from: string]: {
+    [to: string]: number;
+  };
+};
+
+const parseLine = (line: string): Valve & { rawNeighbors: string[] } => {
   // Valve AA has flow rate=0; tunnels lead to valves RZ, QQ, FH, IM, VJ
   const regExp = /Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)/;
   const [_, valve, flow, otherValves] = regExp.exec(line);
@@ -26,122 +30,90 @@ const parseLine = (line: string): ValveInfo & { rawNeighbors: string[] } => {
     flowPerMin: toInt(flow),
     neighbors: [],
     rawNeighbors: otherValves.split(", "),
-    distance: {},
-    tmax: {},
   };
 };
 
-const loadValves = (): ValveInfo[] => {
-  const valves = readLines(INPUT)
-    .map((line) => parseLine(line))
-    .sort((a, b) => b.flowPerMin - a.flowPerMin);
-
+const loadValves = (): Valve[] => {
+  const valves = readLines(INPUT).map((line) => parseLine(line));
   // link to other ValveInfo objects
   valves.forEach((valve) => {
     valve.neighbors = valve.rawNeighbors.map((n) => valves.find((v2) => v2.name === n));
     delete valve.rawNeighbors;
   });
+  return valves;
+};
 
-  // compute distances between all valves
-  valves.forEach((v) => {
-    v.distance = Object.fromEntries(v.neighbors.map((n) => [n.name, 1]));
-    v.distance[v.name] = 0;
+const computeDistances = (valves: Valve[]): Distances => {
+  const distances: Distances = {};
+
+  valves.forEach((valve) => {
+    const dv = (distances[valve.name] = { [valve.name]: 0 });
+    valve.neighbors.forEach((neighbor) => (dv[neighbor.name] = 1));
   });
 
-  // compute distances between all valves
-  valves.forEach(() => {
-    valves.forEach((v) => {
-      v.neighbors.forEach((n) => {
-        Object.keys(n.distance).forEach((key) => {
-          v.distance[key] = Math.min(v.distance[key] ?? Number.MAX_SAFE_INTEGER, n.distance[key] + 1);
+  // compute distances between every pair of valves
+  range(0, valves.length).forEach(() => {
+    valves.forEach((valve) => {
+      const vDistances = distances[valve.name];
+      valve.neighbors.forEach((neighbor) => {
+        Object.entries(distances[neighbor.name]).forEach(([valveId, nDistanceToValveId]) => {
+          const nDistancePlusOne = nDistanceToValveId === Number.MAX_SAFE_INTEGER ? Number.MAX_SAFE_INTEGER : nDistanceToValveId + 1;
+          vDistances[valveId] = Math.min(vDistances[valveId] ?? Number.MAX_SAFE_INTEGER, nDistancePlusOne);
         });
       });
     });
   });
 
-  // compute theoretical maximum flow for each valve if move from src to target with time remaining
-  valves.forEach((src) => {
-    valves.forEach((target) => {
-      src.tmax[target.name] = range(0, TICKS).map((time) => {
-        return target.flowPerMin * Math.max(0, TICKS - time - src.distance[target.name] - 1);
-      });
-    });
-  });
-
-  return valves;
+  return distances;
 };
 
 const valves = loadValves();
-
-const bestPossibleFlowForValve = perf((dest: ValveInfo, src1: ValveInfo, src2: ValveInfo, time: number) => {
-  // best case flow for unopened v if human or elephant moves from their current pos and opens valve
-  return Math.max(src1.tmax[dest.name][time], src2.tmax[dest.name][time]);
-}, "bpffv");
-
-const tmax = perf((currentFlow: number, time: number, unopened: ValveInfo[], src1: ValveInfo, src2: ValveInfo) => {
-  const scores = unopened.map((v) => bestPossibleFlowForValve(v, src1, src2, time)).sort((a, b) => a - b);
-  return scores.slice((0 - TICKS - time) / 2).reduce((acc, x) => acc + x, 0) + currentFlow;
-}, "tmax");
-
-type Context = {
-  time: number;
-  totalFlow: number;
-  theoreticalMax: number;
-  pos1: ValveInfo;
-  pos2: ValveInfo;
-  unopened: ValveInfo[];
-};
-
-const startValve: ValveInfo = valves.find((v) => v.name === "AA");
+const distances = computeDistances(valves);
+type Context = { time: number; totalFlow: number; pos1: Valve; pos2: Valve; unopened: Valve[] };
+const startValve: Valve = valves.find((v) => v.name === "AA");
 const start: Context = {
   time: 0,
   totalFlow: 0,
-  theoreticalMax: 0,
   pos1: startValve,
   pos2: startValve,
-  unopened: valves.filter((x) => x !== startValve && x.flowPerMin > 0),
+  unopened: valves.filter((x) => x.flowPerMin > 0),
 };
-start.theoreticalMax = tmax(start.totalFlow, start.time, start.unopened, start.pos1, start.pos2);
 
-const flowRate = (valve: ValveInfo, time: number) => Math.max(0, valve.flowPerMin * (TICKS - time));
+const routeScore = perf((route: Valve[]) => {
+  return route.reduce(
+    (acc, valve, idx) => {
+      const flow = flowRate(valve, acc.time);
+      const distance = distances[acc.last.name][valve.name];
+      const elapsed = distance + (idx === 0 ? 0 : 1);
+      return { last: valve, time: acc.time - elapsed, flow: acc.flow + flow };
+    },
+    { last: route[0], time: TICKS, flow: 0 }
+  ).flow;
+}, "routeScore");
 
-const next = perf(function next(context: Context): Context[] {
-  const { pos1, pos2 } = context;
-  const time = context.time + 1;
-  const moves1 = context.unopened.includes(pos1) ? [...pos1.neighbors, pos1] : pos1.neighbors;
-  const moves2 = context.unopened.includes(pos2) && pos1 !== pos2 ? [...pos2.neighbors, pos2] : pos2.neighbors;
+type Route = {
+  valves: Valve[];
+  desc: string;
+  score: number;
+};
+const routeToString = (route: Valve[]) => route.map((v) => v.name).join(" -> ");
+// Returns true if the dest valve is reachable (and can be opened) within timeRemaining ticks
+const isReachable = (src: Valve, dest: Valve, timeRemaining: number) => distances[src.name][dest.name] < timeRemaining + 1;
+const flowRate = (valve: Valve, time: number) => Math.max(0, valve.flowPerMin * (TICKS - time));
+const allPaths = (start: Valve, unopened: Valve[], timeRemaining: number): Valve[][] => {
+  const nonTerminal = unopened.filter((v) => isReachable(start, v, timeRemaining));
+  if (nonTerminal.length === 0) return [[start]];
+  const routes = nonTerminal
+    .map((v) => allPaths(v, without(unopened, v), timeRemaining - distances[start.name][v.name] + 1))
+    .flat()
+    .map((childRoute) => [start, ...childRoute]);
+  return routes;
+};
 
-  const result: Context[] = [];
-  moves1.forEach((move1) => {
-    moves2.forEach((move2) => {
-      if (move1 === pos1 && move2 === pos2) {
-        // both opening different valves
-        const unopened = without(context.unopened, pos1, pos2);
-        const totalFlow = context.totalFlow + flowRate(pos1, time) + flowRate(pos2, time);
-        const theoreticalMax = tmax(totalFlow, time, unopened, move1, move2);
-        result.push({ time, pos1, pos2, unopened, totalFlow, theoreticalMax });
-      } else if (move1 === pos1) {
-        // human opening valve, elephant moving
-        const unopened = without(context.unopened, pos1);
-        const totalFlow = context.totalFlow + flowRate(pos1, time);
-        const theoreticalMax = tmax(totalFlow, time, unopened, move1, move2);
-        result.push({ time, pos1: move1, pos2: move2, unopened, totalFlow, theoreticalMax });
-      } else if (move2 === pos2) {
-        // elephant opening valve, human moving
-        const unopened = without(context.unopened, pos2);
-        const totalFlow = context.totalFlow + flowRate(pos2, time);
-        const theoreticalMax = tmax(totalFlow, time, unopened, move1, move2);
-        result.push({ time, pos1: move1, pos2: move2, unopened, totalFlow, theoreticalMax });
-      } else {
-        // both moving
-        const { unopened, totalFlow } = context;
-        const theoreticalMax = tmax(totalFlow, time, unopened, move1, move2);
-        result.push({ time, pos1: move1, pos2: move2, unopened, totalFlow, theoreticalMax });
-      }
-    });
-  });
-  return result;
-});
+const makeRoute = (valves: Valve[]): Route => ({ valves, desc: routeToString(valves), score: routeScore(valves) });
+const routes: Route[] = allPaths(start.pos1, start.unopened, TICKS - start.time).map(makeRoute);
+
+routes.forEach((route) => console.log(`${route.desc}: ${route.score}`));
 
 let best: Context = { ...start, time: TICKS, totalFlow: 0 };
 const setBest = (newBest: Context) => {
@@ -149,37 +121,6 @@ const setBest = (newBest: Context) => {
   console.log({ best: best.totalFlow });
 };
 
-const iterate = perf(function iterate() {
-  const context = priorityQueue.pop();
-  const skipRemaining = context.theoreticalMax <= best.totalFlow;
-  const moves = skipRemaining ? [] : next(context);
-  const header = () =>
-    `${JSON.stringify({
-      depth: priorityQueue.length,
-      contexttime: context.time,
-      tmax: context.theoreticalMax,
-      best: best.totalFlow,
-    })}`;
-  showPerf(10000, header);
-  moves.forEach((result) => {
-    if (result.totalFlow > best.totalFlow) {
-      setBest(result);
-    }
-    if (result.time < TICKS) {
-      priorityQueue.push(result);
-    }
-  });
-});
-
-const contextCompare = (a: Context, b: Context) => {
-  return a.time * a.theoreticalMax - b.time * b.theoreticalMax;
-};
-
-const priorityQueue = new PQ<Context>(contextCompare);
-priorityQueue.push(start);
-
-while (priorityQueue.length) {
-  iterate();
-}
-
+const stack = [];
+stack.push(start);
 console.log({ bestest: best.totalFlow });
