bce508b274941abc9988d7ba20fb5d4fae536e76
Chris Thielen <github@sandgnat.com>
2022-12-24 13:44:45 -1000

    feat(perf): Add @Perf() method decorator

--- 2022/perf.ts
+++ 2022/perf.ts
@@ -1,6 +1,8 @@
-import { lpad } from "./util";
+import { lpad, max, rpad } from "./util";
+import * as yargs from "yargs";
 
-export const PERF = { enabled: false };
+export const PERF = { enabled: yargs.argv["perf"] === true };
+if (PERF.enabled) process.on("beforeExit", showPerf);
 const data: { [key: string]: { count: number; elapsed: number } } = {};
 
 export function perf<ARG extends Function>(fn: ARG, fnname = fn.name): ARG {
@@ -19,17 +21,35 @@ export function perf<ARG extends Function>(fn: ARG, fnname = fn.name): ARG {
   return _perf as any as ARG;
 }
 
+// Class Method Decorator
+export function Perf(fnname?: string) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const getName = () => {
+      const className = target?.constructor?.name;
+      const methodName = fn.name;
+      return `${className}.${methodName}`;
+    };
+    const fn = descriptor.value;
+    descriptor.value = perf(fn, fnname ?? getName());
+    return descriptor;
+  };
+}
+
 let lastShowPerf = Date.now();
 
 const fmt0 = Intl.NumberFormat("default", { useGrouping: true, maximumFractionDigits: 0 });
-const fmt3 = Intl.NumberFormat("default", { useGrouping: true, maximumFractionDigits: 6, minimumFractionDigits: 6 });
+const fmt2 = Intl.NumberFormat("default", { useGrouping: true, maximumFractionDigits: 2, minimumFractionDigits: 2 });
+const fmt6 = Intl.NumberFormat("default", { useGrouping: true, maximumFractionDigits: 6, minimumFractionDigits: 6 });
 const boot = Date.now();
 
+const PADDING1 = 15;
+const PADDING2 = 20;
+
 export function showPerf(interval = -1, header?: () => string) {
+  if (!PERF.enabled) return;
+  const padding1 = Math.max(PADDING1, max(Object.keys(data).map((str) => str.length)) ?? 0);
   const now = Date.now();
   if (now - lastShowPerf > interval) {
-    const PADDING1 = 15;
-    const PADDING2 = 20;
     lastShowPerf = now;
 
     if (header) {
@@ -37,7 +57,7 @@ export function showPerf(interval = -1, header?: () => string) {
     }
     // Render headings
     console.log(
-      lpad(PADDING1, `elapsed: ${fmt0.format(Math.floor((Date.now() - boot) / THOUSAND))}s`) +
+      lpad(padding1, `elapsed: ${fmt0.format(Math.floor((Date.now() - boot) / THOUSAND))}s`) +
         lpad(PADDING2, `Count`) +
         lpad(PADDING2, `Total (sec)`) +
         lpad(PADDING2, `Average (ms)`) +
@@ -49,11 +69,11 @@ export function showPerf(interval = -1, header?: () => string) {
       .sort((a, b) => a.localeCompare(b))
       .forEach((key) => {
         console.log(
-          lpad(PADDING1, key) +
+          rpad(padding1, key) +
             lpad(PADDING2, `${fmt0.format(data[key].count)}`) + // count
-            lpad(PADDING2, `${fmt3.format(data[key].elapsed / BILLION)}`) + // total
-            lpad(PADDING2, `${fmt3.format(data[key].elapsed / data[key].count / MILLION)}`) + // avg
-            lpad(PADDING2, `${fmt0.format((data[key].count / data[key].elapsed) * BILLION)}`) // rate
+            lpad(PADDING2, `${fmt6.format(data[key].elapsed / BILLION)}`) + // total
+            lpad(PADDING2, `${fmt6.format(data[key].elapsed / data[key].count / MILLION)}`) + // avg
+            lpad(PADDING2, `${fmt2.format((data[key].count / data[key].elapsed) * BILLION)}`) // rate
         );
       });
   }
