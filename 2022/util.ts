c6bb69fbd90118eb69cf01d11fad5e39da0edf90
Chris Thielen <github@sandgnat.com>
2022-12-28 13:39:33 -1000

    feat: add productN() helper

--- 2022/util.ts
+++ 2022/util.ts
@@ -10,17 +10,53 @@ export const uniqUsingIsEqualR = <T>(acc: T[], item: T): T[] => {
   }
   return acc;
 };
-export const product = <T, T2>(array1: T[], array2: T2[]) => array1.map((val1) => array2.map((val2) => [val1, val2])).flat(1) as [T, T2][];
-// export const combine2 = <T>(items: T[]) => combine(items, 2) as Array<[T, T]>;
-// export const combine3 = <T>(items: T[]) => combine(items, 3) as Array<[T, T, T]>;
-// export const combine = <T>(items: T[], count: number): Array<T[]> => {
-//   if (count === 1) return items.map((x) => [x]);
-//   if (items.length < count) return [];
-//   return items.reduce((acc, item, idx) => {
-//     const rest = combine(items.slice(idx + 1), count - 1);
-//     return acc.concat(rest.map((x) => [items[idx], ...x]));
-//   }, []);
-// };
+
+// All combinations of: each value from array1 and each value from array2
+// given: product(['a', 'b', 'c'], ['1', '2', '3'])
+// returns:
+// ['a', '1'],['a', '2'], ['a', '3'],
+// ['b', '1'],['b', '2'], ['b', '3'],
+// ['c', '1'],['c', '2'], ['c', '3'],
+export const product = <T1, T2>(array1: T1[], array2: T2[]): [T1, T2][] =>
+  array1.map((val1) => array2.map((val2) => [val1, val2])).flat(1) as [T1, T2][];
+
+// returns a list of arrays, each array containing all values from base and one value from appendValues
+// given: productAppend(['a', 'b', 'c'], ['1', '2', '3'])
+// returns:
+// ['a', 'b', 'c', '1']
+// ['a', 'b', 'c', '2']
+// ['a', 'b', 'c', '3']
+export const productAppend = <T>(base: T[], appendValues: T[]): T[][] => appendValues.map((val2) => [...base, val2]);
+
+// Takes one element from each of N source arrays and returns a list of arrays (of size N) of all combinations
+// (one from Column A, one from Column B, one from Column C, etc)
+// given: productN([-1, 0, 1], [-1, 0, 1], [-1, 0, 1])
+// returns:
+// [-1, -1, -1], [-1, 0, -1], [-1, 1, -1],
+// [ 0, -1, -1], [ 0, 0, -1], [ 0, 1, -1],
+// [ 1, -1, -1], [ 1, 0, -1], [ 1, 1, -1],
+// [-1, -1,  0], [-1, 0,  0], [-1, 1,  0],
+// [ 0, -1,  0], [ 0, 0,  0], [ 0, 1,  0],
+// [ 1, -1,  0], [ 1, 0,  0], [ 1, 1,  0],
+// [-1, -1,  1], [-1, 0,  1], [-1, 1,  1],
+// [ 0, -1,  1], [ 0, 0,  1], [ 0, 1,  1],
+// [ 1, -1,  1], [ 1, 0,  1], [ 1, 1,  1],
+export const productN = <T>(a: T[], b: T[], ...rest: T[][]): T[][] => {
+  const ab = product(a, b);
+  return rest.reduce((acc, nextArray) => acc.map((a1) => productAppend(a1, nextArray)).flat(), ab);
+};
+
+export const combine2 = <T>(items: T[]) => combine(items, 2) as Array<[T, T]>;
+export const combine3 = <T>(items: T[]) => combine(items, 3) as Array<[T, T, T]>;
+// given a list of values, combine the values into all possible groups of #count
+export const combine = <T>(items: T[], count: number): Array<T[]> => {
+  if (count === 1) return items.map((x) => [x]);
+  if (items.length < count) return [];
+  return items.reduce((acc, item, idx) => {
+    const rest = combine(items.slice(idx + 1), count - 1);
+    return acc.concat(rest.map((x) => [items[idx], ...x]));
+  }, []);
+};
 
 export const max = (numbers: number[]) => numbers.reduce((acc, x) => Math.max(acc, x), numbers[0]);
 export const min = (numbers: number[]) => numbers.reduce((acc, x) => Math.min(acc, x), numbers[0]);
